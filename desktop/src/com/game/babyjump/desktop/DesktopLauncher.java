d5a1ab32b8d64a9979d1acbc5243915f8c2ce7d7
Cortland Walker <cortland1568@gmail.com>
2015-10-13 14:27:00 -0500

    Screen Size, Assets, and game Settings

--- desktop/src/com/game/babyjump/desktop/DesktopLauncher.java
+++ desktop/src/com/game/babyjump/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.game.babyjump.BJGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Baby Jump";
+		config.width = 420;
+		config.height = 800;
 		new LwjglApplication(new BJGame(), config);
 	}
 }
